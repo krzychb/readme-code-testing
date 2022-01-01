f40297f0805b5d4a2597a98570ccf3fa35895ce5
Krzysztof <krzychb@gazeta.pl>
2022-01-01 21:39:08 +0800

    src: another round of updates and conflict resolution

--- src/mul.py
+++ src/mul.py
@@ -1,13 +1,5 @@
 from math import pow
 
 def multiple(x):
-<<<<<<< HEAD
-<<<<<<< HEAD
     # sample function to show
-=======
-    // sample function
->>>>>>> Update mul.py
-=======
-    # sample function
->>>>>>> Update mul.py
     return pow(x, 2)
