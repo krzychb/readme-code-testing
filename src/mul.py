b5c72ec653f6b690e89ff53d5e1e66c29ff0ffd6
Krzysztof <krzychb@users.noreply.github.com>
2022-01-01 20:54:05 +0800

    Update mul.py

--- src/mul.py
+++ src/mul.py
@@ -2,8 +2,12 @@ from math import pow
 
 def multiple(x):
 <<<<<<< HEAD
+<<<<<<< HEAD
     # sample function to show
 =======
     // sample function
+>>>>>>> Update mul.py
+=======
+    # sample function
 >>>>>>> Update mul.py
     return pow(x, 2)
